31c4afbececcb9b0fd0f08a9497446393d573a5c
Fscripter <61314160+Fscripter@users.noreply.github.com>
2023-09-27 19:30:07 -0500

    Sesion 27/09 Part 2

--- dom.js
+++ dom.js
@@ -31,3 +31,49 @@ botonConsignar.style.height = "200px";
 titulo.appendChild(botonConsignar);
 
 botonConsignar.className = "arcoiris";
+
+botonConsignar.addEventListener("click", () => {
+  console.log("Has hecho click");
+});
+
+let saldo = 500;
+
+let boton1 = document.getElementById("add"); //Aumentar
+let boton2 = document.getElementById("minus"); //Disminuir
+let saldoText = document.getElementById("Saldo");
+let transacciones = document.getElementById("transacciones");
+
+function actualizarSaldo() {
+  saldoText.innerHTML = `saldo: $${saldo}`;
+}
+
+boton1.addEventListener("click", () => {
+  saldo += 10;
+  let p = document.createElement("p");
+  p.innerHTML = "Consigno $10 pesos";
+  transacciones.appendChild(p);
+  actualizarSaldo();
+});
+
+boton2.addEventListener("click", () => {
+  let p = document.createElement("p");
+  p.innerHTML = "retiro $10 pesos";
+  saldo -= 10;
+  transacciones.appendChild(p);
+  actualizarSaldo();
+});
+
+//Nueva funcionalidad
+
+let realizarConsignacion = document.getElementById("consignar");
+
+realizarConsignacion.addEventListener("click", () => {
+  event.preventDefault();
+  let cuentaConsignacion = document.getElementById("cuentaConsignar");
+  saldo -= 10;
+  let p = document.createElement("p");
+  p.innerHTML = `Consignaste $10 a la cuenta ${cuentaConsignacion.value}`;
+
+  transacciones.appendChild(p);
+  actualizarSaldo();
+});
